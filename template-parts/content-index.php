92295f5a2048e894f021d28dc6376a9254c3d881
Fritz <fre.healy@gmail.com>
2017-12-21 12:18:55 -0500

    styling homepage

--- template-parts/content-index.php
+++ template-parts/content-index.php
@@ -12,6 +12,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class("template-index"); ?> 
 style="background-image: url(<?php echo get_template_directory_uri()."/images/bg-grad.png";?>">
     <div class="wrapper cap">
-        <?php include get_template_directory()."/images/landingpage-logo.svg";?>
+        <?php include get_template_directory()."/images/landingpage-logo.php";?>
     </div><!--.wrapper.cap-->
 </article><!-- #post-## -->
