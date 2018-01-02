3c4bc897d016ab721346333f875cd11f1b304be9
Fritz <fre.healy@gmail.com>
2018-01-02 15:00:40 -0500

    adding sitemap and styilng 404 templates

---
+++ template-parts/content-sitemap.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ACStarter
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-page"); ?>>
+    <div class="wrapper cap">
+        <section class="row-1">
+            <header>
+                <h1><?php the_title();?></h1>
+                <?php $date = get_field("date");
+                if($date):?>
+                    <h2><?php echo $date;?></h2>
+                <?php endif;?>
+            </header>
+            <div class="copy">
+                <?php the_content();?>
+                <?php wp_nav_menu( array( 'theme_location' => 'sitemap' ) );?> 
+            </div><!--.copy-->
+        </section><!--.row-1-->
+    </div><!--.wrapper.cap-->
+</article><!-- #post-## -->
